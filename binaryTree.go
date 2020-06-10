5b79c027f6d42f5b3468d09ad0afb1fcfde24327
Richard Bailey <richard@richardbailey.xyz>
2020-06-10 22:41:22 +0100

    Binary tree maze generation

---
+++ binaryTree.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func binaryTree(g *Grid) {
+	g.forEachCellDo(func(c *Cell) bool {
+		neighbors := []*Cell{}
+		if c.n != nil {
+			neighbors = append(neighbors, c.n)
+		}
+		if c.e != nil {
+			neighbors = append(neighbors, c.e)
+		}
+
+		if len(neighbors) > 0 {
+			s := rand.NewSource(time.Now().UnixNano())
+			r := rand.New(s)
+			rn := r.Intn(len(neighbors))
+			c.Link(neighbors[rn], true)
+		}
+
+		return true
+	})
+}
